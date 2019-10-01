bb5476c126a7185004b902f68ad1d349c22346bf
Tevin Sherman <tevinsherman@yahoo.com>
2019-10-01 11:47:47 -0400

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,11 +1,19 @@
 require 'open-uri'
+require 'Nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open("https://learn-co-curriculum.github.io/student-scraper-test-page/index.html"))
+    doc.css(".roster-cards-container").each do |roster|
+    #binding.pry
+   # Scraper.new.scrape_index_page
+   # div class ="main-wrapper roster"
+   # div class = ".roster-cards-container"
+    end
   end
+  #Scraper.new.scrape_index_page
 
   def self.scrape_profile_page(profile_url)
     
